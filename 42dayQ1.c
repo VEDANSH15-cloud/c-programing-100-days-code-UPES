c10a66bb6a910600af276cd1584157e53eca997d
VEDANSH15-cloud <VEDANSH.32849@stu.upes.ac.in>
2026-09-22 12:29:11 +0530

    daily question

---
+++ 42dayQ1.c
@@ -0,0 +1,35 @@
+//Q83: Count vowels and consonants in a string.
+
+/*
+Sample Test Cases:
+Input 1:
+hello
+Output 1:
+Vowels=2, Consonants=3
+
+*/
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    int vowels = 0, consonants = 0;
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0' && str[i] != '\n'; i++) {
+        char c = tolower(str[i]);
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            vowels++;
+        } else if (isalpha(c)) {
+            consonants++;
+        }
+    }
+
+    printf("Vowels=%d, Consonants=%d\n", vowels, consonants);
+
+    return 0;
+}
